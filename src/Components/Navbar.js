eb38f89885ea5ad26aa0478e6f492c170dce3fd1
TecnnoBot03 <simoncarreno2006@gmail.com>
2024-07-31 19:44:27 -0500

    build: Basic structure and first imports on React

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from "../Assets/Logo.svg"
+import { BsCart2 } from 'react-icons/bs'
+import { HiOutlineBars3 } from 'react-icons/hi2'
+import { Box, Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import HomeIcon from '@mui/icons-material/Home'
+import InfoIcon from '@mui/icons-material/Info'
+import CommentRoundedIcon from '@mui/icons-material/CommentRounded'
+
+const Navbar = () => {
+  return (
+    <div>
+        Navbar
+    </div>
+  )
+}
+
+export default Navbar
