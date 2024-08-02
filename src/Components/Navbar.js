0b8c154f8e63abe523c3e649b5d94c9fa8cb9627
TecnnoBot03 <simoncarreno2006@gmail.com>
2024-08-01 21:15:37 -0500

    build: Import of material icons dependencies

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,6 +6,7 @@ import { Box, Drawer, ListItem, ListItemButton, ListItemIcon, ListItemText } fro
 import HomeIcon from '@mui/icons-material/Home'
 import InfoIcon from '@mui/icons-material/Info'
 import CommentRoundedIcon from '@mui/icons-material/CommentRounded'
+import PhoneRoundedIcon from '@mui/icons-material/PhoneRounded'
 
 const Navbar = () => {
   return (
