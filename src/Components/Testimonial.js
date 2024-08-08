846083614c8285796889119fd830c1c2e8a68f50
Cachumbin <simoncarreno2006@gmail.com>
2024-08-08 16:59:49 -0500

    build: Created the Testimonials component

---
+++ src/Components/Testimonial.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProfilePic from '../Assets/john-doe-image.png'
+import { AiFillStar } from 'react-icons/ai'
+
+
+const Testimonial = () => {
+  return (
+    <div>Testimonial</div>
+  )
+}
+
+export default Testimonial
