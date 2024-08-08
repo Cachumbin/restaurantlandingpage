68040d217861f80353d64d3253c67f8f93800993
Cachumbin <simoncarreno2006@gmail.com>
2024-08-08 17:08:39 -0500

    build: Added the Heading and primary Text to Testimonials Component

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -5,8 +5,20 @@ import { AiFillStar } from 'react-icons/ai'
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <div className='work-section-wrapper'>
+        <div className='work-section-top'>
+            <p className='primary-subheading'>Testimonials</p>
+            <h1 className='primary-heading'>
+                What They Are Saying
+            </h1>
+            <p className="primary-text">
+                Lorem ipsum dolor sit amet consectetur. Non tincidunt 
+                magna non et elit. Dolor  turpis molestie dui
+                magnis facilisis at fringilla quam.
+            </p>
+        </div>
+    </div>
   )
 }
 
-export default Testimonial
+export default Testimonial  
